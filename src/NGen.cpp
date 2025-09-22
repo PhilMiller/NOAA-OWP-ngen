65c3ff72297b375c55f14f072938a001ecf13f5e
Phil Miller <pmiller@lynker.com>
2025-09-22 16:55:53 -0700

    Refactor ngen main() to encapsulate state during execution

--- src/NGen.cpp
+++ src/NGen.cpp
@@ -20,6 +20,8 @@
 #include <boost/algorithm/string.hpp>
 #include <boost/range/algorithm/sort.hpp>
 
+#include <NgenSimulation.hpp>
+
 #ifdef WRITE_PID_FILE_FOR_GDB_SERVER
 #include <unistd.h>
 #endif // WRITE_PID_FILE_FOR_GDB_SERVER
@@ -567,58 +569,15 @@ int main(int argc, char *argv[]) {
     auto time_done_init = std::chrono::steady_clock::now();
     std::chrono::duration<double> time_elapsed_init = time_done_init - time_start;
 
-    //Now loop some time, iterate catchments, do stuff for total number of output times
-    auto num_times = manager->Simulation_Time_Object->get_total_output_times();
-    for( int count = 0; count < num_times; count++) 
-    {
-      // The Inner loop will advance all layers unless doing so will break one of two constraints
-      // 1) A layer may not proceed ahead of the master simulation object's current time
-      // 2) A layer may not proceed ahead of any layer that is computed before it
-      // The do while loop ensures that all layers are tested at least once while allowing 
-      // layers with small time steps to be updated more than once
-      // If a layer with a large time step is after a layer with a small time step the
-      // layer with the large time step will wait for multiple timesteps from the preceeding
-      // layer.
-      
-      // this is the time that layers are trying to reach (or get as close as possible)
-      auto next_time = manager->Simulation_Time_Object->next_timestep_epoch_time();
+    auto simulation = std::make_unique<NgenSimulation>(manager,
+                                                       layers,
+                                                       catchment_indexes,
+                                                       catchment_outflows,
+                                                       nexus_indexes,
+                                                       nexus_downstream_flows);
 
-      // this is the time that the layer above the current layer is at
-      auto prev_layer_time = next_time;
-
-      // this is the time that the least advanced layer is at
-      auto layer_min_next_time = next_time;
-      do
-      {
-        for ( auto& layer : layers ) 
-        {
-          auto layer_next_time = layer->next_timestep_epoch_time();
-
-          // only advance if you would not pass the master next time and the previous layer next time
-          if ( layer_next_time <= next_time && layer_next_time <=  prev_layer_time)
-          {
-            if(count%100==0) std::cout<<"Updating layer: "<<layer->get_name()<<"\n";
-            layer->update_models(); //assume update_models() calls time->advance_timestep()
-            prev_layer_time = layer_next_time;
-          }
-          else
-          {
-            layer_min_next_time = prev_layer_time = layer->current_timestep_epoch_time(); 
-          }
-
-          if ( layer_min_next_time > layer_next_time)
-          {
-            layer_min_next_time = layer_next_time;
-          }
-        } //done layers
-      } while( layer_min_next_time < next_time );  // rerun the loop until the last layer would pass the master next time
-
-      if (count + 1 < num_times)
-      {
-        manager->Simulation_Time_Object->advance_timestep();
-      }
-
-    } //done time
+    // Currently breaks routing data transfer; will move routing down into this
+    simulation->run_catchments();
 
     // Close nexus output file(s)
     nexus_outputs_mgr->close();

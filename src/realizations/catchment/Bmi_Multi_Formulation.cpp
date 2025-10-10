470f8e1d19efdfd8bb2b14a217c177c323240d39
siva.selvanathan <sselvanathan_IE@Dewberry.com>
2025-10-10 11:35:34 -0400

    Added the capability to read and set the output variables information from the realization file in the existing and proposed new format.

--- src/realizations/catchment/Bmi_Multi_Formulation.cpp
+++ src/realizations/catchment/Bmi_Multi_Formulation.cpp
@@ -100,9 +100,9 @@ void Bmi_Multi_Formulation::create_multi_formulation(geojson::PropertyMap proper
     // TODO: get synced end_time values for all models
 
     // Setup formulation output variable subset and order, if present
-    bool old_json_format = false;
+    bool old_format = false;
     std::vector<std::string> out_headers;//define empty vector for headers
-    std::vector<std::string> out_units;//define empty vector for units for new json structure
+    std::vector<std::string> out_units;//define empty vector for headers for new format
     auto out_var_it = properties.find(BMI_REALIZATION_CFG_PARAM_OPT__OUT_VARS);
     if (out_var_it != properties.end()) {
         std::vector<geojson::JSONProperty> out_vars_json_list = out_var_it->second.as_list();
@@ -111,11 +111,11 @@ void Bmi_Multi_Formulation::create_multi_formulation(geojson::PropertyMap proper
         if (out_vars_json_list.size() > 0){
             std::string item_type = get_propertytype_name(out_vars_json_list[0].get_type());
             if (item_type == "String"){
-                old_json_format = true;
+                old_format = true;
             }
         }
         std::vector<std::string> out_vars(out_vars_json_list.size());
-        if (old_json_format){
+        if (old_format){
             for (int i = 0; i < out_vars_json_list.size(); ++i) {
                 out_vars[i] = out_vars_json_list[i].as_string();
             }
@@ -138,30 +138,12 @@ void Bmi_Multi_Formulation::create_multi_formulation(geojson::PropertyMap proper
         set_output_variable_names(modules.back()->get_output_variable_names());
     }
 
-    auto out_var_it = properties.find(BMI_REALIZATION_CFG_PARAM_OPT__OUT_NEW_VARS);
-    std::stringstream ss_test;
-    if (out_var_it != properties.end()) {
-        std::vector<geojson::JSONProperty> out_vars_json_list = out_var_it->second.as_list();
-        std::vector<std::string> out_vars(out_vars_json_list.size());
-        for (int i = 0; i < out_vars_json_list.size(); ++i) {
-            out_vars[i] = out_vars_json_list[i].as_string();
-            ss_test << "Output variable name: " << out_vars[i] << std::endl;
-            LOG(ss_test.str(), LogLevel::INFO); ss_test.str("");
-        }
-        set_output_variable_names(out_vars);
-    }
-    // Otherwise, for multi BMI, the BMI output variables of the last nested module should be used.
-    else {
-        //is_out_vars_from_last_mod = true;
-        //set_output_variable_names(modules.back()->get_output_variable_names());
-    }
-    
     // TODO: consider warning if nested module formulations have formulation output variables, as that level of the
     //  config is (at present) going to be ignored (though strictly speaking, this doesn't apply to the last module in
     //  a certain case).
 
     // Output header fields, if present
-    if(old_json_format){
+    if(old_format){
         auto out_headers_it = properties.find(BMI_REALIZATION_CFG_PARAM_OPT__OUT_HEADER_FIELDS);
         if (out_headers_it != properties.end()) {
             std::vector<geojson::JSONProperty> out_headers_json_list = out_headers_it->second.as_list();
@@ -188,9 +170,7 @@ void Bmi_Multi_Formulation::create_multi_formulation(geojson::PropertyMap proper
     else{
         //in new format, if headers are not set. 
         //This happens when the the BMI output variables of the last nested module should be used.
-        if(out_headers.size() == 0){
-            set_output_header_fields(get_output_variable_names());
-        }
+        set_output_header_fields(get_output_variable_names());
     }
 
     // Output precision, if present

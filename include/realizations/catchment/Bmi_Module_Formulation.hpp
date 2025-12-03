a8a604fca9d4a8937dadfed4864584c3a3dee9c2
siva.selvanathan <sselvanathan_IE@Dewberry.com>
2025-12-03 12:36:33 -0500

    Updated signature for get_bmi_native_units to allow get_bmi_output_var_name function call.

--- include/realizations/catchment/Bmi_Module_Formulation.hpp
+++ include/realizations/catchment/Bmi_Module_Formulation.hpp
@@ -254,7 +254,7 @@ namespace realization {
 
         const std::vector<std::string> get_bmi_input_variables() const override;
         const std::vector<std::string> get_bmi_output_variables() const override;
-        const std::string get_bmi_native_units(const std::string &name) const override;
+        const std::string get_bmi_native_units(const std::string &name) override;
 
         virtual void check_mass_balance(const int& iteration, const int& total_steps, const std::string& timestamp) const override {
             //Create the protocol context, each member is const, and cannot change during the check

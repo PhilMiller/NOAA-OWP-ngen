08a055af0e325001815399375d2f2e7550118574
Ian Todd <itodd@dewberry.com>
2025-12-08 13:14:29 -0500

    Remove explicit interpreter release

--- src/NGen.cpp
+++ src/NGen.cpp
@@ -789,8 +789,6 @@ int run_ngen(int argc, char* argv[], int mpi_num_procs, int mpi_rank) {
         simulation->save_end_of_run(snapshot);
     }
 
-    _interp.reset();
-
     simulation->finalize();
     manager->finalize();
 
